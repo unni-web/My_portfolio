eb2829594dd35a48d7f07f28c1ed6e1a325cd873
unni-web <unnikrish1011@gmail.com>
2026-02-15 06:03:58 +0530

    completed

--- portfolio/src/Components/Layout/Navbar.jsx
+++ portfolio/src/Components/Layout/Navbar.jsx
@@ -65,7 +65,7 @@ const Navbar = () => {
             {/* RIGHT: Contact Button */}
             <div className="flex-1 flex items-center justify-end">
                 <a
-                    href="/public/Unnikrishnan-CV.pdf"
+                    href="/public/Resume.pdf"
                     className="px-3 py-2 bg-indigo-700 text-white  rounded-xl hover:bg-indigo-600 transition"
                 >
                     Download CV
